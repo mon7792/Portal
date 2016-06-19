86e7ae867dd2be7658a5315c65885b77660cc42a
monish <monish7792@gmail.com>
2016-06-19 17:33:53 +0530

    The Initial Setup2: Added a bootstrap static files  and working Working bootstrap webpage.

--- portalproj/settings/base.py
+++ portalproj/settings/base.py
@@ -121,13 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static_in_env", "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, "static","static_root")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_in_pro", "our_static"),
+    os.path.join(BASE_DIR, "static","static_dirs"),
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "static_in_env", "media_root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media_root")
 
 
 
